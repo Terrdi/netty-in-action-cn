9cd93cc802af35e92ca766474f55f66211d1a0ef
Terrdi <675464934@qq.com>
2020-11-11 15:13:18 +0800

    BlockingIoExample 增加关闭流操作

--- chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
+++ chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
@@ -17,7 +17,8 @@ public class BlockingIoExample {
     /**
      * 代码清单 1-1 阻塞 I/O 示例
      * */
-    public void serve(int portNumber) throws IOException {
+    public void serve(int portNumber) {
+        try(
         //创建一个新的 ServerSocket，用以监听指定端口上的连接请求
         ServerSocket serverSocket = new ServerSocket(portNumber);
         //对accept()方法的调用将被阻塞，直到一个连接建立
@@ -26,19 +27,23 @@ public class BlockingIoExample {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);
-        String request, response;
-        //处理循环开始
-        while ((request = in.readLine()) != null) {
-            if ("Done".equals(request)) {
-                break;
+                new PrintWriter(clientSocket.getOutputStream(), true);){
+            String request, response;
+            //处理循环开始
+            while ((request = in.readLine()) != null) {
+                if ("Done".equals(request)) {
+                    break;
+                }
+                //请求被传递给服务器的处理方法
+                response = processRequest(request);
+                //服务器的响应被发送给了客户端
+                out.println(response);
+                //继续执行处理循环
             }
-            //请求被传递给服务器的处理方法
-            response = processRequest(request);
-            //服务器的响应被发送给了客户端
-            out.println(response);
-            //继续执行处理循环
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
     private String processRequest(String request){
